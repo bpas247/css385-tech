6e7831b514dc95b0913f9f996603ce847496bcc4
Jere111115 <sjbrothers@comcast.net>
2019-04-26 13:59:28 -0700

    Added infected prefab

---
+++ Assets/Scripts/InfectedScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectedScript : MonoBehaviour
+{
+
+    public GameObject cells;
+    public float MoveSpeed = 6.0f;
+    int currentPlayer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayer = Random.Range(0, cells.transform.childCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(cells.transform.childCount > 0)
+        {
+            transform.LookAt(cells.transform.GetChild(currentPlayer));
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Healthy_cell")
+        {
+            // TODO: Do not destory object, it is currently place holder for testing
+            // Actual implementation will have chance of infecting healthy
+            Destroy(collision.gameObject);
+            currentPlayer = Random.Range(0, cells.transform.childCount - 1);
+            
+        }
+    }
+}
