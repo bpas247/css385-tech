3ec384c35c9ab5d7641b3b9e5d66bdb910216849
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-04-26 20:24:48 -0700

    Add infected (#9)
    * Added infected prefab
    
    * Fixed out of bounds error

---
+++ Assets/Scripts/InstantiateInfected.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateInfected : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public GameObject healthyCells;
+    public int amount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            GameObject infectedInstance = Instantiate(prefab, new Vector3(Random.Range(-10, 10), 1, Random.Range(-10, 10)), Quaternion.identity);
+            infectedInstance.GetComponent<InfectedScript>().cells = healthyCells;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
